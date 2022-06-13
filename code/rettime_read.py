7a523b9e57b3b94b02e7cc1206fd31305473166a
Arturo Abril <arturo.abril06@gmail.com>
2022-06-13 14:11:44 +0200

    nose

--- code/rettime_read.py
+++ code/rettime_read.py
@@ -3,11 +3,11 @@ from aux import *
 import sys
 import pylab as pl
 
-with open("./out/values_tau_d1.npy", "rb") as file_tau:
+with open("values_tau_d10000.npy", "rb") as file_tau:
     a = np.load(file_tau)
     constants = np.load(file_tau)
     tau_values = np.load(file_tau)
-    with open("./out/values_tau0_30.npy", "rb") as file_tau0:
+    with open("values_tau0.npy", "rb") as file_tau0:
         if all(constants == np.load(file_tau0)):
             tau0_values = np.load(file_tau0)
         else:
@@ -16,6 +16,7 @@ with open("./out/values_tau_d1.npy", "rb") as file_tau:
 r = float(constants[0])
 L = int(constants[1])
 size = int(constants[2])
+print(size)
 rmin = float(constants[3])
 rmax = float(constants[4])
 smin = float(constants[5])
@@ -24,7 +25,7 @@ smax = float(constants[6])
 rho_values = np.linspace(rmin, rmax, size) 
 sigma_values = np.linspace(smin, smax, size) 
 
-contours = pl.contour(sigma_values, rho_values, np.log10(tau_values), 6, colors = "black")
+contours = pl.contour(sigma_values, rho_values, np.log10(tau_values), 5, colors = "black")
 pl.clabel(contours, inline=True, fontsize=8)
 
 
@@ -34,7 +35,7 @@ pl.clabel(contours, inline=True, fontsize=8)
 # contour_boundary = pl.contour(sigma_values, rho_values, diff_matrix, [8], colors = "magenta", linewidths = 3, linestyles = "dashed")
 ####
 
-pl.imshow(np.log10(tau_values), extent = [smin, smax, rmin, rmax], origin = "lower", cmap = "RdGy", alpha = .5, interpolation = "bilinear")
+pl.imshow(np.log10(tau_values), extent = [smin, smax, rmin, rmax], origin = "lower", cmap = "RdGy", alpha = .5, interpolation = "none")
 
 pl.colorbar(label = "Tiempo de recuperación, " + r"$\log_{10}\tau$", drawedges = False)
 
